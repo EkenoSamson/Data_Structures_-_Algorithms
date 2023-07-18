6c9d390fdd56fa9a1747bf8d322573148c07c775
EkenoSamson <samsonekeno98@gmail.com>
2023-07-18 08:14:14 -0700

    hello roomate

---
+++ 0x00-First_C/02L01.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Description: first c program
+ * Return: nothing
+ *
+ */
+
+int main(void)
+{
+	printf("Akula-Ng'imoe, roomate!\r\nThis is my first C program.\n");
+
+	return (0);
+}
---
+++ 0x00-First_C/02L02.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - entry point.
+ * Description: display a message.
+ * Return: 0
+ *
+ */
+
+int main(void)
+{
+	printf("It's fun to write my own program in C.\r\n");
+
+	return (0);
+}
